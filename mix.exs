b945472f04d55f32f33b5779e1e941371bb4b538
Miguel Palhas <mpalhas@gmail.com>
2017-05-10 10:36:53 +0100

    Removing some comments

--- mix.exs
+++ mix.exs
@@ -1,7 +1,6 @@
 defmodule Mse.Mixfile do
   use Mix.Project
 
-
   def project do
     [apps_path: "apps",
      version: "alpha.10",
@@ -13,18 +12,6 @@ defmodule Mse.Mixfile do
      deps: deps()]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
     [
       {:credo, "~> 0.7", only: [:dev, :test]},
