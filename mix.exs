2c38570946624a9172c523ee2a71bf8e912facad
Miguel Palhas <mpalhas@gmail.com>
2017-05-12 11:43:17 +0100

    alpha.12

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mse.Mixfile do
 
   def project do
     [apps_path: "apps",
-     version: "alpha.11",
+     version: "alpha.12",
      apps: [
        :db, :mse_web, :graph, :mtgio,
      ],
