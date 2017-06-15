da19b621062089d84be6eeec55912bc0428c382b
Miguel Palhas <mpalhas@gmail.com>
2017-06-15 18:46:01 +0100

    Fixing error in mkm controller

--- apps/mse_web/lib/mse_web/controllers/admin/mkm_controller.ex
+++ apps/mse_web/lib/mse_web/controllers/admin/mkm_controller.ex
@@ -2,7 +2,7 @@ defmodule Mse.Web.Admin.MKMController do
   use Mse.Web, :controller
   import ExAdmin.Utils
 
-  def update(conn) do
+  def update(conn, _params) do
     Exq.enqueue(Exq, "default", Workers.Admin.MKMImport, [])
 
     redirect(conn, to: admin_path())
