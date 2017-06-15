28f80d263361e4e79ccc7775c9fa11cbc7cdd37c
Miguel Palhas <mpalhas@gmail.com>
2017-06-15 16:55:28 +0100

    Bumps to version alpha.13

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mse.Mixfile do
 
   def project do
     [apps_path: "apps",
-     version: "alpha.12",
+     version: "alpha.13",
      apps: [
        :db, :workers, :mse_web, :graph, :gatherer, :mkm_api, :mtgjson
      ],
