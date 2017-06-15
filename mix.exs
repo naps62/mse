24df2e0ee30ac84b47174c43b0de0a7fe492817b
Miguel Palhas <mpalhas@gmail.com>
2017-06-15 22:11:13 +0100

    alpha.14

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mse.Mixfile do
 
   def project do
     [apps_path: "apps",
-     version: "alpha.17",
+     version: "alpha.18",
      apps: [
        :db, :workers, :mse_web, :graph, :gatherer, :mkm_api, :mtgjson
      ],
