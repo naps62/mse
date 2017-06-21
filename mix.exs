5d75e72d42e5e5844ceb7453b83fc437b1ba03b3
Miguel Palhas <mpalhas@gmail.com>
2017-06-21 22:42:51 +0100

    Fixing mtgjson update job

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mse.Mixfile do
 
   def project do
     [apps_path: "apps",
-     version: "alpha.34",
+     version: "alpha.35",
      apps: [
        :db, :workers, :mse_web, :graph, :gatherer, :mkm_api, :mtgjson, :mse_logging
      ],
