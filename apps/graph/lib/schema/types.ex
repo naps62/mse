bdcb92a7377c9462935d3bd9a4f6beb05edb36d6
Miguel Palhas <mpalhas@gmail.com>
2017-08-13 00:21:39 +0100

    A couple more fields to graphql schema

--- apps/graph/lib/schema/types.ex
+++ apps/graph/lib/schema/types.ex
@@ -7,6 +7,11 @@ defmodule Graph.Schema.Types do
     serialize &Poison.encode!/1
   end
 
+  scalar :utc_datetime, description: "UTC Datetime" do
+    parse &to_string/1
+    serialize &Ecto.DateTime.cast!/1
+  end
+
   object :set do
     field :id, :integer
     field :name, :string
@@ -15,8 +20,12 @@ defmodule Graph.Schema.Types do
     field :gatherer_code, :string
 
     field :mkm_data, :json
+    field :mkm_updated_at, :utc_datetime
     field :mtgjson_data, :json
+    field :gatherer_code, :string
+    field :mtgjson_codes, list_of(:string)
     field :gatherer_data, :json
+    field :gatherer_updated_at, :utc_datetime
 
     field :cards, list_of(:card), resolve: assoc(:cards)
   end
@@ -33,10 +42,19 @@ defmodule Graph.Schema.Types do
 
     field :gatherer_id, :string
 
+    field :single_id, :integer
+
     field :mkm_basic_data, :json
     field :mkm_detailed_data, :json
+    field :mkm_double_faced, :boolean
+    field :mkm_back_face, :boolean
+    field :mkm_basic_updated_at, :utc_datetime
+    field :mkm_detailed_updated_at, :utc_datetime
+    field :mtgjson_id, :string
     field :mtgjson_data, :json
+    field :gatherer_id, :string
     field :gatherer_data, :json
+    field :gatherer_updated_at, :utc_datetime
 
     field :set, :set
     field :single, :single
@@ -54,8 +72,12 @@ defmodule Graph.Schema.Types do
     field :image_url, :string
 
     field :mkm_data, :json
+    field :mkm_double_faced, :boolean
+    field :mkm_back_face, :boolean
+    field :mkm_updated_at, :utc_datetime
     field :mtgjson_data, :json
     field :gatherer_data, :json
+    field :gatherer_updated_at, :utc_datetime
 
     field :cards, list_of(:card), resolve: assoc(:card)
   end
