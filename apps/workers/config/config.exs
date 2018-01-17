509f4f2ade790a9c0ce60abbc8355244ecac6866
Miguel Palhas <mpalhas@gmail.com>
2018-01-17 23:04:55 +0000

    Typo fix

--- apps/workers/config/config.exs
+++ apps/workers/config/config.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :workers, Workers.Scheduler,
   jobs: [
-    {"@hourly", {Workers.Recurrent.PriceUpdater, :run, [500]}}
+    {"@hourly", {Workers.Recurrent.PriceUpdater, :run, [500]}},
     {"@daily", {Workers.Admin.MKMImport, :perform, []}}
   ]
