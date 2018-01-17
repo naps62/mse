2592583955c03d304ecf8ef98cace8bc68bc5054
Miguel Palhas <mpalhas@gmail.com>
2018-01-17 23:03:19 +0000

    Adding daily MKM updates

--- apps/workers/config/config.exs
+++ apps/workers/config/config.exs
@@ -3,4 +3,5 @@ use Mix.Config
 config :workers, Workers.Scheduler,
   jobs: [
     {"@hourly", {Workers.Recurrent.PriceUpdater, :run, [500]}}
+    {"@daily", {Workers.Admin.MKMImport, :perform, []}}
   ]
