e4e2151b83511885a8ec94ad5b8ec9dd985a727f
Miguel Palhas <mpalhas@gmail.com>
2018-03-07 01:04:46 +0000

    Hotfix

--- apps/mkm_api/lib/mkm_api/cards_basic/save.ex
+++ apps/mkm_api/lib/mkm_api/cards_basic/save.ex
@@ -9,6 +9,7 @@ defmodule MkmApi.CardsBasic.Save do
         new_card = %Card{}
         single_id = single_id_for(new_card, card_data)
         SilentRepo.insert(card_changeset(new_card, card_data, set, single_id))
+
       card ->
         single_id = single_id_for(card, card_data)
         SilentRepo.update(card_changeset(card, card_data, set, single_id))
@@ -18,14 +19,14 @@ defmodule MkmApi.CardsBasic.Save do
   defp find_card(%{"idProduct" => mkm_id}) do
     Card
     |> where([c], c.mkm_id == ^mkm_id)
-    |> SilentRepo.one
+    |> SilentRepo.one()
   end
 
   defp card_changeset(card, data, set, single_id) do
     change(card)
     |> put_change(:mkm_basic_data, data)
     |> put_change(:mkm_id, data["idProduct"])
-    |> put_change(:mkm_basic_updated_at, Timex.now)
+    |> put_change(:mkm_basic_updated_at, Timex.now())
     |> put_change(:mkm_double_faced, double_faced?(data))
     |> put_change(:set_id, set.id)
     |> put_change(:single_id, single_id)
@@ -40,15 +41,16 @@ defmodule MkmApi.CardsBasic.Save do
     Regex.match?(~r|\s/{1,2}\s|, name)
   end
 
-  defp single_id_for(%Card{single_id: nil}, data), do:
-    Single
-    |> where([s], s.mkm_id == ^data["idMetaproduct"])
-    |> select([s], s.id)
-    |> SilentRepo.one
-  defp single_id_for(%Card{single_id: id}, _, _, _), do:
-    id
+  defp single_id_for(%Card{single_id: nil}, data),
+    do:
+      Single
+      |> where([s], s.mkm_id == ^data["idMetaproduct"])
+      |> select([s], s.id)
+      |> SilentRepo.one()
+
+  defp single_id_for(%Card{single_id: id}, _), do: id
+  defp single_id_for(%Card{single_id: id}, _, _, _), do: id
 
   defp mkm_relative_url("." <> relative_url), do: mkm_relative_url(relative_url)
-  defp mkm_relative_url(relative_url), do:
-    "https://mkmapi.eu" <> relative_url
+  defp mkm_relative_url(relative_url), do: "https://mkmapi.eu" <> relative_url
 end
