d645964346945512a04a7d522824b3d5ff6c2f99
Miguel Palhas <mpalhas@gmail.com>
2018-06-04 23:14:31 +0100

    Update more sets

--- config/config.exs
+++ config/config.exs
@@ -1,18 +1,10 @@
-# This file is responsible for configuring your application
-#
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# By default, the umbrella project as well as each child
-# application will require this configuration file, ensuring
-# they all use the same configuration. While one could
-# configure all applications here, we prefer to delegate
-# back to each application for organization purposes.
 import_config "../apps/*/config/config.exs"
 
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
 
-if Mix.env == :prod or Mix.env == :dev do
+if Mix.env() == :prod or Mix.env() == :dev do
   import_config "mkm-prod.exs"
 end
 
